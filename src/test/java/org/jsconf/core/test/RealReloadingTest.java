f303dbd504fe4b6a31192b4195fd5d9927378672
Yves Galante <yyvess@gmail.com>
2015-03-18 21:27:48 +0100

    Improve WatchResource

--- src/test/java/org/jsconf/core/test/RealReloadingTest.java
+++ src/test/java/org/jsconf/core/test/RealReloadingTest.java
@@ -1,80 +1,1 @@
-package org.jsconf.core.test;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-
-import org.jsconf.core.ConfigurationFactory;
-import org.jsconf.core.sample.bean.ConfigBean;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.test.annotation.Repeat;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.support.AnnotationConfigContextLoader;
-
-// Test required to have put ${java.io.tmpdir} on ClassPath
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(loader = AnnotationConfigContextLoader.class)
-public class RealReloadingTest {
-
-    private static final String TIC = "Tic";
-    private static final String TAC = "Tac";
-
-    private static File tempFile;
-
-    @Autowired
-    private ConfigBean conf;
-
-    @Autowired
-    private ConfigurationFactory factory;
-
-    @BeforeClass
-    public static void init() throws IOException {
-        String prefix = Long.toString(System.nanoTime());
-        tempFile = File.createTempFile(prefix, "_app.conf");
-        tempFile.deleteOnExit();
-    }
-
-    @Test
-    @Repeat
-    // Test required to have put ${java.io.tmpdir} on ClassPath
-    public void testRealReloading() throws IOException, InterruptedException {
-        final Object ref = this.conf;
-        assertNotNull(this.conf);
-
-        assertEquals(null, this.conf.getUrl());
-        write(TIC);
-        assertEquals(TIC, this.conf.getUrl());
-        write(TAC);
-        assertEquals(TAC, this.conf.getUrl());
-        write(TIC);
-        assertEquals(TIC, this.conf.getUrl());
-
-        assertTrue(ref == this.conf);
-    }
-
-    private void write(String value) throws IOException, InterruptedException {
-        try (FileWriter fw = new FileWriter(tempFile)) {
-            fw.append("simpleConf : {  url : \"" + value + "\" }");
-        }
-        Thread.sleep(1000);
-    }
-
-    @Configuration
-    static class ContextConfiguration {
-        @Bean
-        public static ConfigurationFactory configurationFactory() {
-            return new ConfigurationFactory().withResourceName(tempFile.getName())//
-                    .withBean("simpleConf", ConfigBean.class, "myBeanId", true);
-        }
-    }
-}
+package org.jsconf.core.test;import org.jsconf.core.ConfigurationFactory;import org.jsconf.core.sample.bean.ConfigBean;import org.junit.AfterClass;import org.junit.BeforeClass;import org.junit.Test;import org.junit.runner.RunWith;import org.springframework.beans.factory.annotation.Autowired;import org.springframework.context.annotation.Bean;import org.springframework.context.annotation.Configuration;import org.springframework.test.context.ContextConfiguration;import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;import org.springframework.test.context.support.AnnotationConfigContextLoader;import java.io.File;import java.io.FileWriter;import java.io.IOException;import java.nio.file.Files;import java.nio.file.Path;import java.nio.file.Paths;import static org.junit.Assert.assertEquals;import static org.junit.Assert.assertNotNull;import static org.junit.Assert.assertTrue;// Test required to have put ${java.io.tmpdir} on ClassPath@RunWith(SpringJUnit4ClassRunner.class)@ContextConfiguration(loader = AnnotationConfigContextLoader.class)public class RealReloadingTest {    private static final String TIC = "Tic";    private static final String TAC = "Tac";  private static Path tempDirectory;  private static Path tempFile;    @Autowired    private ConfigBean conf;    @BeforeClass    public static void init() throws IOException {      tempDirectory = Files.createTempDirectory("jsconf-".concat(Long.toString(System.nanoTime())));      tempFile = Files.createFile(Paths.get(tempDirectory.toString(), "app.conf"));    }  @AfterClass  public static void clean() throws IOException {    Files.deleteIfExists(tempFile);    Files.deleteIfExists(tempDirectory);  }    @Test    // Test required to have put ${java.io.tmpdir} on ClassPath    public void testRealReloading() throws IOException, InterruptedException {        final Object ref = this.conf;        assertNotNull(this.conf);        assertEquals(null, this.conf.getUrl());        write(TIC);        assertEquals(TIC, this.conf.getUrl());        write(TAC);        assertEquals(TAC, this.conf.getUrl());        assertTrue(ref == this.conf);      Files.deleteIfExists(tempFile);      Files.createFile(Paths.get(tempDirectory.toString(), "app.conf"));      write(TIC);      assertEquals(TIC, this.conf.getUrl());                    }    private void write(String value) throws IOException, InterruptedException {      try (FileWriter fw = new FileWriter(new File(tempFile.toUri()))) {        fw.append("simpleConf : {  url : \"").append(value).append("\" }");        }        Thread.sleep(1000);    }    @Configuration    static class ContextConfiguration {        @Bean        public static ConfigurationFactory configurationFactory() {          return new ConfigurationFactory().withResourceName(                  tempDirectory.getName(tempDirectory.getNameCount() - 1) + "/app.conf")//                    .withBean("simpleConf", ConfigBean.class, "myBeanId", true);        }    }}
