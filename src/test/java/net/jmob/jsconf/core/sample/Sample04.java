d4c1bd5352e1d97c4c9689381f252f38de190433
Yves Galante <yves.galante@jmob.net>
2020-11-28 11:14:56 +0100

    feat: Migration to Java 8 - Update dependencies

--- src/test/java/net/jmob/jsconf/core/sample/Sample04.java
+++ src/test/java/net/jmob/jsconf/core/sample/Sample04.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2013 Yves Galante
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,31 +17,32 @@
 package net.jmob.jsconf.core.sample;
 
 import net.jmob.jsconf.core.ConfigFormat;
-import org.apache.commons.dbcp.BasicDataSource;
 import net.jmob.jsconf.core.ConfigurationFactory;
 import net.jmob.jsconf.core.sample.bean.Sequence;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.apache.commons.dbcp.BasicDataSource;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+
+@ExtendWith(SpringExtension.class)
 @ContextConfiguration(loader = AnnotationConfigContextLoader.class)
-public class Sample04 {
+class Sample04 {
 
     @Autowired
     private Sequence sequence;
 
     @Test
-    public void test() {
-        Assert.assertNotNull(this.sequence);
+    void test() {
+        assertNotNull(this.sequence);
         assertEquals("SEQ_NAME", this.sequence.getName());
         assertEquals("org.apache.commons.dbcp.BasicDataSource", this.sequence.getDataSource()
                 .getClass().getName());
@@ -50,7 +51,7 @@ public class Sample04 {
     @Configuration
     static class ContextConfiguration {
         @Bean
-        public static ConfigurationFactory configurationFactory() {
+        static ConfigurationFactory configurationFactory() {
             return new ConfigurationFactory()
                     .withResourceName("net/jmob/jsconf/core/sample/app_04.conf")
                     .withFormat(ConfigFormat.CONF)
