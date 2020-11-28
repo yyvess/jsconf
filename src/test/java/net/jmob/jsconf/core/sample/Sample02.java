29d5223a9ef5b63cce84d56d414a673a09e4c551
Yves Galante <yves.galante@jmob.net>
2020-11-28 11:14:56 +0100

    BREAKING CHANGE: Migration to Java 8 - Update dependency

--- src/test/java/net/jmob/jsconf/core/sample/Sample02.java
+++ src/test/java/net/jmob/jsconf/core/sample/Sample02.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2013 Yves Galante
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,28 +18,29 @@ package net.jmob.jsconf.core.sample;
 
 import net.jmob.jsconf.core.ConfigurationFactory;
 import net.jmob.jsconf.core.sample.bean.ConfigBean;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+
+@ExtendWith(SpringExtension.class)
 @ContextConfiguration(loader = AnnotationConfigContextLoader.class)
-public class Sample02 {
+class Sample02 {
 
     @Autowired
     // This interface not required implementation
     private ConfigBean conf;
 
     @Test
-    public void test() {
+    void test() {
         assertNotNull(this.conf);
         assertEquals("Hello World", this.conf.getUrl());
         assertEquals(12, this.conf.getPort());
@@ -53,7 +54,7 @@ public class Sample02 {
     @Configuration
     static class ContextConfiguration {
         @Bean
-        public static ConfigurationFactory configurationFactory() {
+        static ConfigurationFactory configurationFactory() {
             return new ConfigurationFactory()
                     .withResourceName("net/jmob/jsconf/core/sample/app_02")
                     .withScanPackage(ConfigBean.class.getPackage().getName());
--- src/test/java/net/jmob/jsconf/core/test/VirtualBeanTest.java
+++ src/test/java/net/jmob/jsconf/core/test/VirtualBeanTest.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2013 Yves Galante
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,11 +16,11 @@
 
 package net.jmob.jsconf.core.test;
 
-import net.jmob.jsconf.core.test.bean.ConfigByInterface;
 import net.jmob.jsconf.core.ConfigurationFactory;
 import net.jmob.jsconf.core.impl.VirtualBean;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import net.jmob.jsconf.core.test.bean.ConfigByInterface;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.BeanCreationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -29,16 +29,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jmx.access.InvocationFailureException;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 import java.util.HashMap;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+
+@ExtendWith(SpringExtension.class)
 @ContextConfiguration(loader = AnnotationConfigContextLoader.class)
-public class VirtualBeanTest {
+class VirtualBeanTest {
 
     @Autowired
     @Qualifier("simpleConf")
@@ -51,7 +52,7 @@ public class VirtualBeanTest {
     private ConfigByInterface conf3;
 
     @Test
-    public void testHashCode() {
+    void testHashCode() {
         assertEquals(-1831264279, this.conf.hashCode());
         assertEquals(this.conf.hashCode(), this.conf2.hashCode());
         assertEquals(-1828017501, this.conf3.hashCode());
@@ -59,12 +60,12 @@ public class VirtualBeanTest {
     }
 
     @Test
-    public void testEquals() {
-        assertNotEquals(this.conf, "");
-        assertNotEquals(this.conf, null);
+    void testEquals() {
+        assertNotEquals("", this.conf);
+        assertNotNull(this.conf);
         assertEquals(this.conf, this.conf);
         assertEquals(new VirtualBean(null), new VirtualBean(null));
-        assertEquals(new VirtualBean(new HashMap<String, Object>()), new VirtualBean(new HashMap<String, Object>()));
+        assertEquals(new VirtualBean(new HashMap<>()), new VirtualBean(new HashMap<>()));
         assertNotEquals(this.conf, new VirtualBean(null));
         assertNotEquals(new VirtualBean(null), this.conf);
         assertNotSame(this.conf, this.conf2);
@@ -74,37 +75,38 @@ public class VirtualBeanTest {
     }
 
     @Test
-    public void testToString() {
+    void testToString() {
         assertEquals("{getUrl=https://localhost/Tic}", this.conf.toString());
     }
 
     @Test
-    public void testGet() {
+    void testGet() {
         assertEquals("https://localhost/Tic", this.conf.getUrl());
         assertEquals("xxx", this.conf3.getX());
     }
 
-    @Test(expected = InvocationFailureException.class)
-    public void testSet() {
-        this.conf3.setX("yyy");
+    @Test
+    void testSet() {
+        assertThrows(InvocationFailureException.class, () -> this.conf3.setX("yyy"));
     }
 
     @Test
-    public void validationSuccess() {
+    void validationSuccess() {
         new AnnotationConfigApplicationContext(ContextConfiguration.class)
                 .getBean("simpleConf");
     }
 
-    @Test(expected = BeanCreationException.class)
-    public void validationFailed() {
-        new AnnotationConfigApplicationContext(ContextConfigurationValidationFailed.class)
-                .getBean("simpleConf");
+    @Test
+    void validationFailed() {
+        assertThrows(BeanCreationException.class, () ->
+                new AnnotationConfigApplicationContext(ContextConfigurationValidationFailed.class)
+                        .getBean("simpleConf"));
     }
 
     @Configuration
-    static class ContextConfiguration {
+    public static class ContextConfiguration {
         @Bean
-        public static ConfigurationFactory configurationFactory() {
+        static ConfigurationFactory configurationFactory() {
             return new ConfigurationFactory()
                     .withResourceName("net/jmob/jsconf/core/test/app_virtual")
                     .withBean("simpleConf", ConfigByInterface.class)
@@ -113,9 +115,9 @@ public class VirtualBeanTest {
         }
     }
 
-    static class ContextConfigurationValidationFailed {
+    public static class ContextConfigurationValidationFailed {
         @Bean
-        public static ConfigurationFactory configurationFactory() {
+        static ConfigurationFactory configurationFactory() {
             return new ConfigurationFactory()
                     .withResourceName("net/jmob/jsconf/core/test/app_virtual_failed")
                     .withBean("simpleConf", ConfigByInterface.class);
